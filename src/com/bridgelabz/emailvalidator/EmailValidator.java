5958ed5ccf10a60223178fd88c8385928fb5ac4c
namancg <naman.c@perfios.com>
2021-09-07 15:47:48 +0530

    ADDED: method to welcome user

---
+++ src/com/bridgelabz/emailvalidator/EmailValidator.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.emailvalidator;
+public class EmailValidator {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("PERFORMING EMAIL VALIDATION USING REGEX");
+
+	}
+
+}
