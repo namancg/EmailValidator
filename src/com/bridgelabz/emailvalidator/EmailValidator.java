65d67f2f59347e2bcad1e2c1b78542fb65b79598
namancg <naman.c@perfios.com>
2021-09-07 16:33:44 +0530

    ADDED: method to make sure email is valid

--- src/com/bridgelabz/emailvalidator/EmailValidator.java
+++ src/com/bridgelabz/emailvalidator/EmailValidator.java
@@ -1,10 +1,32 @@
 package com.bridgelabz.emailvalidator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 public class EmailValidator {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("PERFORMING EMAIL VALIDATION USING REGEX");
+		String emailId = "abc@bridgelabz.com";
+		checkValidEmail(emailId);
+		emailId = ".abc@abc.com";
+		checkValidEmail(emailId);
+		emailId = "_343namanchandra1999@gmail.com";
+		checkValidEmail(emailId);
+		
 
+	}
+	public static void checkValidEmail(String email) 
+	{
+		String regexCheck= "^[a-zA-z][.@a-zA-Z]*$";
+		Pattern pattern = Pattern.compile(regexCheck);
+		Matcher obj= pattern.matcher(email);
+		if(obj.matches()==true)
+		{
+		System.out.println("THIS IS A VALID EMAIL");
+			
+		}
+		else
+			System.out.println("NOT A VALID EMAIL");
+		
 	}
 
 }
