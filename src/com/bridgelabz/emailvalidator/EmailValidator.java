bbec4432de60c829a20cb7afb5228d5204c6a939
namancg <naman.c@perfios.com>
2021-09-07 17:40:58 +0530

    REFACTORED: method to validate email with specified special characters

--- src/com/bridgelabz/emailvalidator/EmailValidator.java
+++ src/com/bridgelabz/emailvalidator/EmailValidator.java
@@ -13,6 +13,19 @@ public class EmailValidator {
 		checkValidEmail(emailId);
 		emailId = "abc@bridgelabz.com";
 		checkValidEmail(emailId);
+		emailId = "abc@yahoo.com";
+		checkValidEmail(emailId);
+		emailId = "abc@1.com";
+		checkValidEmail(emailId);
+		emailId = "abc-100@yahoo.com";
+		checkValidEmail(emailId);
+		emailId = "abc.100@yahoo.com";
+		checkValidEmail(emailId);
+		emailId = "abc111@abc.com";
+		checkValidEmail(emailId);
+		emailId = "abc-100@abc.net";
+		checkValidEmail(emailId);
+		
 		emailId = ".abc@abc.com";
 		checkValidEmail(emailId);
 		emailId = "abc@123@gmaila";
@@ -21,12 +34,24 @@ public class EmailValidator {
 		checkValidEmail(emailId);
 		emailId = "abc123@gmaila";
 		checkValidEmail(emailId);
+		emailId = "abc";
+		checkValidEmail(emailId);
+		emailId = "abc..2002@gmail.com";
+		checkValidEmail(emailId);
+		emailId = "abc@abc@gmail.com";
+		checkValidEmail(emailId);
+		emailId = "abc()*@gmail.com";
+		checkValidEmail(emailId);
+		emailId = "abc@%*@gmail.com";
+		checkValidEmail(emailId);
+		
+
 
 
 	}
 	public static void checkValidEmail(String email) 
 	{
-		String regexCheck= "^[a-zA-z]*[@][.a-z0-9]+[.]{1}[.a-z]*$";
+		String regexCheck= "^[a-zA-z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[.a-z]*$";
 		Pattern pattern = Pattern.compile(regexCheck);
 		Matcher obj= pattern.matcher(email);
 		if(obj.matches()==true)
